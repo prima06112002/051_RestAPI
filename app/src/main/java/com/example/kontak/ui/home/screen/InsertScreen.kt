554a524a0f8632f927507540ec01d662c0cd2ddc
Prima06112002 <ramadhan06112002@gmail.com>
2023-12-28 18:37:04 +0700

    menambahkan if (enabled) di fun forminputsiswa

--- app/src/main/java/com/example/kontak/ui/home/screen/InsertScreen.kt
+++ app/src/main/java/com/example/kontak/ui/home/screen/InsertScreen.kt
@@ -8,18 +8,27 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.kontak.navigation.DestinasiNavigasi
+import com.example.kontak.ui.PenyediaViewModel
+import com.example.kontak.ui.TopAppBarKontak
 import com.example.kontak.ui.home.viewmodel.InsertUiEvent
+import com.example.kontak.ui.home.viewmodel.InsertViewModel
 
 object DestinasiEntry : DestinasiNavigasi {
     override val route = "item_entry"
     override val titleRes = "Entry Siswa"
 }
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -60,7 +69,12 @@ fun FormInputSiswa(
             modifier = Modifier.fillMaxWidth(),
             enabled = enabled
         )
-
+        if (enabled) {
+            Text(
+                text = "Isi semua data",
+                modifier = Modifier.padding(start = 12.dp)
+            )
+        }
         Divider(
             thickness = 8.dp,
             modifier = Modifier.padding(bottom = 12.dp)
