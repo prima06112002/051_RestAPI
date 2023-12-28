757d893a693d4a18110390554b067a5565934c49
Prima06112002 <ramadhan06112002@gmail.com>
2023-12-28 18:46:28 +0700

    perbaikan di KontakApp.kt

--- app/src/main/java/com/example/kontak/ui/KontakApp.kt
+++ app/src/main/java/com/example/kontak/ui/KontakApp.kt
@@ -12,23 +12,18 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.kontak.R
 import com.example.kontak.navigation.PengelolaHalaman
-import com.example.kontak.ui.home.screen.HomeScreen
-import com.example.kontak.ui.home.viewmodel.HomeViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun KontakApp(
-    homeViewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory )
 ){
     val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
     Scaffold(
