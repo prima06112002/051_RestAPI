d4d2945afd19e6e7c0af5876ce30f90d0fbc01b3
Prima06112002 <ramadhan06112002@gmail.com>
2023-12-28 18:26:01 +0700

    menambahkan insertviewmodel di penyediaviewmodel

--- app/src/main/java/com/example/kontak/ui/PenyediaViewModel.kt
+++ app/src/main/java/com/example/kontak/ui/PenyediaViewModel.kt
@@ -6,13 +6,18 @@ import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.kontak.KontakApplication
 import com.example.kontak.ui.home.viewmodel.HomeViewModel
+import com.example.kontak.ui.home.viewmodel.InsertViewModel
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer {
             HomeViewModel(aplikasiMars().container.kontakRepository)
         }
+        initializer {
+            InsertViewModel(aplikasiMars().container.kontakRepository)
+        }
     }
+
+    fun CreationExtras.aplikasiMars(): KontakApplication =
+        (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as KontakApplication)
 }
-fun CreationExtras.aplikasiMars(): KontakApplication =
-    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY]as KontakApplication)
